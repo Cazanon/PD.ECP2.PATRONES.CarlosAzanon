08b7ea49b32be137ffc140c62acf4d8e57f02ffd
Carlos <Carlos@Xapa_Desktop>
2014-10-22 23:53:48 +0200

    Clases de estados creadas

---
+++ src/main/java/state/connection/EstadoCerrado.java
@@ -0,0 +1,47 @@
+package state.connection;
+
+public class EstadoCerrado extends Estado {
+
+    @Override
+    public void abrir(Conexion conexion) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void cerrar(Conexion conexion) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void enviar(String msg, Link link, Conexion conexion) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void recibir(int respuesta, Conexion conexion) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void iniciar(Conexion conexion) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void parar(Conexion conexion) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
