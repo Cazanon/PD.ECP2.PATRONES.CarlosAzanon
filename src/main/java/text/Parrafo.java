67ca9f970d22d70ddef6f4460551e2f37cd728d5
Carlos <Carlos@Xapa_Desktop>
2014-10-23 17:01:24 +0200

    Implementado y testeado text

--- src/main/java/text/Parrafo.java
+++ src/main/java/text/Parrafo.java
@@ -1,17 +1,31 @@
 package text;
 
+import java.util.ArrayList;
+
 public class Parrafo extends Componente {
 
+    private ArrayList<Caracter> parrafo;
+    
+    public Parrafo(){
+        parrafo=new ArrayList<Caracter>();
+    }
+    
     @Override
-    public void add(Componente componente) {
-        // TODO Auto-generated method stub
-        
+    public void add(Componente componente){
+        try{
+            this.parrafo.add((Caracter)componente);         
+        }catch(ClassCastException e1){
+            throw new UnsupportedOperationException();
+        }       
     }
 
     @Override
-    public String dibujar(boolean mayusculas) {
-        // TODO Auto-generated method stub
-        return null;
+    public String dibujar(boolean mayusculas){
+        String parraf="";
+        for(Caracter caracter: parrafo) {
+            parraf+=caracter.dibujar(mayusculas);
+        }
+        return parraf+"\n";
     }
 
 }
